ae1ba90a7dbdcee1b28bb00131b6e9a6def39f3d
Scipia <kuznetcov.artem.va@gmail.com>
2021-10-03 06:41:28 +0300

    first commit

---
+++ cmd/main/app.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"TOD/internal/user"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	log.Println("create router")
+	router := httprouter.New()
+
+	log.Println("register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	log.Println("start application")
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:           router,
+		WriteTimeout:      15 * time.Second,
+		ReadTimeout: 	   15 * time.Second,
+	}
+
+	log.Fatal(server.Serve(listener))
+}
